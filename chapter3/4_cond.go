4bfb60ecabcee790fec498e6408ceee522f8b279
lapeko <lapeko88@gmail.com>
2025-06-29 11:57:30 +0200

    Cond example

---
+++ chapter3/4_cond.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	size := 10
+	wg.Add(size)
+	list := make([]struct{}, 0, 10)
+	cond := sync.Cond{L: &sync.Mutex{}}
+
+	pop := func() {
+		defer func() {
+			cond.Signal()
+			cond.L.Unlock()
+			wg.Done()
+		}()
+
+		cond.L.Lock()
+		fmt.Println("Pop from list")
+		list = list[1:]
+	}
+
+	for i := 0; i < size; i++ {
+		cond.L.Lock()
+		for len(list) >= 2 {
+			cond.Wait()
+		}
+		fmt.Println("Pushing into list")
+		list = append(list, struct{}{})
+		cond.L.Unlock()
+
+		go pop()
+	}
+	wg.Wait()
+}
